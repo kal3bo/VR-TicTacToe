9a577492f5771b883bd00913c5ca92eae9b6d766
Kaleb <d.kal3b@gmail.com>
2020-08-23 20:08:13 -0700

    Realtime Objects

---
+++ Assets/0_VRTicTacToe/Scripts/RealtimeThrowable.cs
@@ -0,0 +1,27 @@
+﻿using Normal.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+
+public class RealtimeThrowable : MonoBehaviour
+{
+    private RealtimeTransform realtimeTransform;
+    private RealtimeView realtimeView;
+    public int ownership = -1;
+
+    private void Start()
+    {
+        realtimeTransform = gameObject.GetComponent<RealtimeTransform>();
+        realtimeView = gameObject.GetComponent<RealtimeView>();
+    }
+
+    public void GrabbedObject()
+    {
+        realtimeTransform.RequestOwnership();
+        realtimeView.RequestOwnership();
+        ownership = realtimeTransform.ownerID;
+    }
+
+}
