d79eb180fbf098fff1fd6442286fd3d8f56719a2
Kaleb <d.kal3b@gmail.com>
2020-08-25 00:34:57 -0700

    Start Button Functionality

---
+++ Assets/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private GameObject turnBall;
+    private int randomStarter = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("Play");
+        randomStarter = Random.Range(0, 2);
+        turnBall.SetActive(true);
+
+        if(randomStarter == 0)
+        {
+            turnBall.transform.position = new Vector3(15.365f, 1.341f, 2.355f);
+        }
+        else
+        {
+            turnBall.transform.position = new Vector3(15.365f, 1.341f, 1.002f);
+        }
+    }
+}
